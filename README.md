64bcd662781abf0731d09ae7e2af071e579c78b2
AlexandraLamadrid <alexandralamadridcortina@gmail.com>
2025-07-10 16:10:39 -0500

    Subiendo el primer punto

---
+++ README.md
@@ -0,0 +1,1 @@
+Ejercicios laboratorio JS Generation
---
+++ variablesYTiposDeDatos.js
@@ -0,0 +1,19 @@
+//Numero
+let edad = 33;
+console.log("Edad: " + edad);
+
+//Texto
+let nombre = "Alexandra";
+console.log("Nombre: " + nombre);
+
+//Booleano
+let esEstudiante = true;
+console.log("¿Es estudiante?:", esEstudiante);
+
+//Nulo
+let mascota = null;
+console.log("Mascota:", mascota);
+
+//Indefinido
+let direccion;
+console.log("Dirección:", direccion); 
